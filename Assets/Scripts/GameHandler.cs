add9034c92b1aeb1d52a523b4917e0e186d922af
Sean Purcell <me@seanp.xyz>
2019-05-12 17:58:00 -0400

    Follow some simple tutorial for super basic start

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GameHandler.Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
