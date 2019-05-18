11ca22d23c197abed2bb0ceca381fe6fd4a227a6
Sean Purcell <me@seanp.xyz>
2019-05-18 15:10:46 -0400

    Create very simple beam

---
+++ Assets/Scripts/BeamHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamHandler : MonoBehaviour
+{
+    const float SPEED = 0.5f;
+    public  GameHandler game;
+
+    public LineRenderer r;
+    public Vector2 start;
+    public Vector2 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r.SetPosition(0, start);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float len = SPEED * game.SimTime();
+        r.SetPosition(1, len * dir + start);
+        Debug.Log("Len: " + len.ToString());
+    }
+}
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
+    float simTime_;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("GameHandler.Start");
+        simTime_ = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        simTime_ += Time.deltaTime;
+    }
+
+    public float SimTime() {
+        return simTime_;
     }
 }
