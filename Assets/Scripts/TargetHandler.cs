9d42ea6f5b172bbc2e639117fb94826a3b51ac16
Sean Purcell <me@seanp.xyz>
2019-05-30 14:04:44 -0400

    Make target detection based on colliders

--- Assets/Scripts/TargetHandler.cs
+++ Assets/Scripts/TargetHandler.cs
@@ -7,9 +7,12 @@ public class TargetHandler : TileHandler
 
     private Renderer renderer;
 
+    int layerMask;
+
     // Start is called before the first frame update
     void Start()
     {
+        layerMask = 1 << LayerMask.NameToLayer("Beam");
         renderer = GetComponent<Renderer>();
         renderer.material.color = Color.grey;
     }
@@ -17,11 +20,23 @@ public class TargetHandler : TileHandler
     // Update is called once per frame
     void Update()
     {
+        // Check for any collisions with lasers
+
+        var collisions = Physics.OverlapSphere(
+                transform.position,
+                transform.localScale.x / 2,
+                layerMask);
+        foreach (var col in collisions) {
+            Debug.Log("Hit target: " + col.gameObject.name);
+        }
+        if (collisions.Length > 0) {
+            renderer.material.color = Color.cyan;
+        } else {
+            renderer.material.color = Color.grey;
+        }
     }
 
     public override void OnBeamCollision(BeamHandler beam, RaycastHit hit)
     {
-        renderer.material.color = Color.cyan;
-        //game.Win();
     }
 }
