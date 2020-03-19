6a9955d3b5b91417087b9be457767fa5a8c040ed
Zhenyu Yang <zhenyutyang@gmail.com>
2020-03-19 01:34:23 -0400

    recolor stick figure; stick figure completely hides gvr reticle

--- Assets/Scripts/CrosshairHandler.cs
+++ Assets/Scripts/CrosshairHandler.cs
@@ -7,6 +7,8 @@ public class CrosshairHandler : MonoBehaviour
 {
     public Image playerMovementImage;
 
+    public MeshRenderer gvrReticleRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +18,28 @@ public class CrosshairHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         
     }
 
     public void ShowPlayerMovement()
     {
         playerMovementImage.enabled = true;
+        HideReticle();
     }
 
     public void HidePlayerMovement()
     {
         playerMovementImage.enabled = false;
+        ShowReticle();
+    }
+
+    public void ShowReticle()
+    {
+        gvrReticleRenderer.enabled = true;
+    }
+
+    public void HideReticle()
+    {
+        gvrReticleRenderer.enabled = false;
     }
 }
